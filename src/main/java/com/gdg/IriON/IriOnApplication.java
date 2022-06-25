dec09ce10ecb1e35253bb5d312e48002825d3b3b
choigy1001 <easytwo10@naver.com>
2022-06-25 19:48:06 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/gdg/IriON/IriOnApplication.java
@@ -0,0 +1,13 @@
+package com.gdg.IriON;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IriOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IriOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdg/IriON/IriOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdg.IriON;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IriOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
